a581879d7322f7d67f34d0a03265ee8b94d4d6b4
Luis Herrera <lherrera@thoughtworks.com>
2019-09-21 17:16:21 -0500

    moves txt files

--- circleci_execute.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-docker run --rm -t -v $(pwd):/data \
-    -w /data \
-    --privileged \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    herreraluis/circleci-alpine local execute
--- circleci_validate.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run --rm -v $(pwd):/data circleci/circleci-cli:alpine config validate /data/.circleci/config.yml --token $TOKEN_CIRCLECI
