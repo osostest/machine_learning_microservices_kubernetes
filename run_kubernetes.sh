f16706e550b654ed1fad13836be65b80d7f8d2e0
Luis Herrera <lherrera@thoughtworks.com>
2019-09-21 18:26:46 -0500

    adds logs command in kubernetes script and login step in docker script

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 dockerpath=herreraluis/machine-learning-udacity
-# Step 2
+
 # Run the Docker Hub container with kubernetes
-kubectl run --image=$dockerpath machine-learning-app --port=80 
-# Step 3:
+kubectl run --image=$dockerpath machine-learning-app --port=80 --labels='app=machine-learning-app'
+
 # List kubernetes pods
 kubectl get pods
-# Step 4:
+
 # Forward the container port to a host
 kubectl port-forward --address 0.0.0.0 deployment/machine-learning-app 8000:80
+
+# Shows logs of containers
+kubectl logs -lapp=machine-learning-app --all-containers=true
--- upload_docker.sh
+++ upload_docker.sh
@@ -2,4 +2,5 @@
 
 dockerpath=herreraluis/machine-learning-udacity
 echo "Docker ID and Image: $dockerpath"
+docker login
 docker push $dockerpath
