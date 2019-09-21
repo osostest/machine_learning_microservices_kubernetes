79b109f19dcda7316b5fef105be4ef9999cc4a42
Luis Herrera <lherrera@thoughtworks.com>
2019-09-21 16:54:38 -0500

    adds kubernetes script for deploy containers

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -1,18 +1,11 @@
 #!/usr/bin/env bash
-
-# This tags and uploads an image to Docker Hub
-
-# Step 1:
-# This is your Docker ID/path
-# dockerpath=<>
-
+dockerpath=herreraluis/machine-learning-udacity
 # Step 2
 # Run the Docker Hub container with kubernetes
-
-
+kubectl run --image=$dockerpath machine-learning-app --port=80 
 # Step 3:
 # List kubernetes pods
-
+kubectl get pods
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward --address 0.0.0.0 deployment/machine-learning-app 8000:80
