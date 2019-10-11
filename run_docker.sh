40bff6c8065430654a71ce37d2907cdeda694704
Luis Herrera <lherrera@thoughtworks.com>
2019-10-11 15:17:23 -0500

    adds scripts for run app in kubernetes

--- run_docker.sh
+++ run_docker.sh
@@ -3,4 +3,4 @@ image_name=herreraluis/machine-learning-udacity
 echo $image_name
 docker build --tag $image_name -f Dockerfile .
 docker images
-docker run -ti --rm -p 8000:80 $image_name
+docker run -ti --rm -p 80:80 $image_name
--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -1,14 +1,21 @@
 #!/usr/bin/env bash
+
 dockerpath=herreraluis/machine-learning-udacity
 
 # Run the Docker Hub container with kubernetes
-kubectl run --image=$dockerpath machine-learning-app --port=80 --labels='app=machine-learning-app'
+kubectl run --generator=run-pod/v1 --image=$dockerpath machine-learning-app --port=80 --labels='app=machine-learning-app'
 
 # List kubernetes pods
 kubectl get pods
 
+# while loop for pod status
+while [ "$(kubectl describe pods machine-learning-app | grep ^Status: | head -1 | awk '{print $2}' | tr -d '\n')" != "Running" ]; do
+    echo "Waiting for POD status: $(kubectl describe pods machine-learning-app | grep ^Status: | head -1 | awk '{print $2}' | tr -d '\n')"
+done
+echo "POD is Running"
+
 # Forward the container port to a host
-kubectl port-forward --address 0.0.0.0 deployment/machine-learning-app 8000:80
+kubectl port-forward --address 0.0.0.0 pod/machine-learning-app 80:80
 
 # Shows logs of containers
 kubectl logs -lapp=machine-learning-app --all-containers=true
